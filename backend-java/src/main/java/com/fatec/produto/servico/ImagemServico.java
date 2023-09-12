0ef826e11617b617aaf93fe6c0881d5d5fcae350
DouglasDans <douglas.dans@gmail.com>
2023-09-12 11:17:43 -0300

    Refatoração - Model do catalogo

--- backend-java/src/main/java/com/fatec/produto/servico/ImagemServico.java
+++ backend-java/src/main/java/com/fatec/produto/servico/ImagemServico.java
@@ -1,24 +1,73 @@
 package com.fatec.produto.servico;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.multipart.MultipartFile;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import com.fatec.produto.model.IImagemRepository;
+import com.fatec.produto.model.IProdutoRepository;
 import com.fatec.produto.model.Imagem;
+import com.fatec.produto.model.Produto;
 
-public class ImagemServico implements IImagemServico{
+public class ImagemServico implements IImagemServico {
+   Logger logger = LogManager.getLogger(this.getClass());
+
+   @Autowired
+   private IImagemRepository imagemRepository;
+
+   @Autowired
+   private IProdutoRepository produtoRepository;
+
    @Override
    public Optional<Imagem> salvar(MultipartFile arquivo, Long id) throws IOException {
-      // TODO Auto-generated method stub
-      return Optional.empty();
+      // **********************************************************
+      // Obter informações sobre o arquivo - espera um tipo formdata chave/file
+      // **********************************************************
+
+      logger.info(">>>>>> servico salvar imagem - iniciado...");
+
+      Optional<Produto> p = produtoRepository.findById(id);
+      
+      if (p.isPresent()) {
+
+         logger.info(">>>>>> servico salvar imagem - produto encontrado");
+
+         String nome = arquivo.getOriginalFilename();
+         Path caminhoArquivo = Paths.get("imagens/" + nome);
+
+         logger.info(">>>>>> servico salvar imagem - caminho arquivo => " + caminhoArquivo);
+
+         Imagem imagem = new Imagem();
+         imagem.setId(id); // associa o id do produto ao id da imagem
+         imagem.setNome(arquivo.getOriginalFilename());
+         imagem.setCaminho(caminhoArquivo.toString());
+         imagem.setArquivo(arquivo.getBytes());
+
+         logger.info(">>>>>> servico salvar imagem - arquivo getSize => " + arquivo.getSize());
+
+         // **********************************************************
+         // salva no disco e no db
+         // ***********************************************************
+         // Files.write(caminhoArquivo, arquivo.getBytes());
+
+         return Optional.of(imagemRepository.save(imagem));
+      } else {
+         logger.info(">>>>>> servico salvar imagem - id nao encontrado");
+         return Optional.empty();
+      }
+
    }
 
    @Override
    public List<Imagem> getAll() {
-      // TODO Auto-generated method stub
-      return null;
+      return imagemRepository.findAll();
    }
 
    @Override
--- backend-java/src/main/java/com/fatec/produto/servico/ProdutoServico.java
+++ backend-java/src/main/java/com/fatec/produto/servico/ProdutoServico.java
@@ -1,21 +1,42 @@
 package com.fatec.produto.servico;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fatec.produto.model.Catalogo;
+import com.fatec.produto.model.IImagemRepository;
 import com.fatec.produto.model.IProdutoRepository;
+import com.fatec.produto.model.Imagem;
 import com.fatec.produto.model.Produto;
 
 @Service
 public class ProdutoServico implements IProdutoServico {
 	@Autowired
-	IProdutoRepository repository;
+	IProdutoRepository repositoryProduto;
+
+	@Autowired
+	IImagemRepository repositoryImagem;
 
 	@Override
-	public List<Produto> consultaCatalogo() {
-		return repository.findAll();
+	public List<Catalogo> consultaCatalogo() {
+		Catalogo c = null;
+		List<Catalogo> lista = new ArrayList<>();
+		List<Produto> listaProduto = repositoryProduto.findAll();
+		List<Imagem> listaImagem = repositoryImagem.findAll();
+
+		for (Produto p : listaProduto) {
+			for (Imagem i : listaImagem) {
+				if (p.getId().equals(i.getId())) {
+					c = new Catalogo(p.getId(), p.getDescricao(), p.getCategoria(),
+							p.getCusto(), p.getQuantidadeNoEstoque(), i.getArquivo());
+					lista.add(c);
+				}
+			}
+		}
+		return lista;
 	}
 
 }
