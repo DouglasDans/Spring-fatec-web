6f702ba7c864433fe3cd0bcf8dd73fa00dfc9fdd
douglasdans <douglas.dans@gmail.com>
2023-09-23 15:20:50 -0300

    Função | Frontend | Adiciona POST na API

--- frontend-react/src/service/API.js
+++ frontend-react/src/service/API.js
@@ -22,3 +22,15 @@ export default async function getAPI(endpoint) {
 
    return obj
 }
+
+async function postAPI(endpoint, body) {
+   const response = await fetch("http://localhost:8080/api/v1/produtos", {
+      method: "POST",
+      headers: {
+         "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+   })
+
+   return response
+}
